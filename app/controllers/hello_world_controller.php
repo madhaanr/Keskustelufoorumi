c61d50e6b5f13fa9a689d8271e4cc9ed19258cfa
madhaanr <marko.haanranta@helsinki.fi>
2015-05-11 14:59:20 +0300

    pientä säätöä

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -9,6 +9,15 @@
 
     public static function sandbox(){
       // Testaa koodiasi täällä
+        $query = DB::connection()->prepare('SELECT * FROM Kayttaja;');
+        $query->execute();
       echo 'Hello World!';
     }
+    
+    public static function hi() {
+//         $query = DB::connection()->prepare('SELECT * FROM Kaytt');
+//        $query->execute();
+        View::make('helloworld.twig');
+    }
+    
   }
