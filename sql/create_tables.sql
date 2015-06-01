aacafad06ebf3be6766496b08a5c66d797f7111a
madhaanr <marko.haanranta@helsinki.fi>
2015-06-01 16:07:11 +0300

    tietokantataulut luotu

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,21 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE F_USER (
+    id SERIAL PRIMARY KEY,
+    username varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
+    role integer
+);
+
+CREATE TABLE F_MESSAGEGROUP (
+    id SERIAL PRIMARY KEY,
+    messagegroup_name varchar(50) NOT NULL
+);
+
+CREATE TABLE F_MESSAGE (
+    id SERIAL PRIMARY KEY,
+    topic varchar(50) NOT NULL,
+    message varchar(1000),
+    added DATE,
+    modified timestamp DEFAULT current_timestamp,
+    messagegroup_id integer REFERENCES F_MESSAGEGROUP(id)
+);
